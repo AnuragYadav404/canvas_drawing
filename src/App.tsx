1d0eae59c4bcae8a131d140d603d580359441121
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2025-07-23 19:41:38 +0200

    Implements Rectangles interactive drawing with storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Canvas } from "./components/Canvas"
 
 function App() {
   return (
-    <div className="h-screen w-screen flex justify-center items-center bg-black text-white">
+    <div className="h-screen w-screen flex justify-center items-center">
       <div>
         <Canvas />
       </div>
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,17 +1,103 @@
 import { useEffect, useRef } from "react"
 
+// interface shape {
+//     startX: number,
+//     startY: number,
+//     endX: number,
+//     endY: number;
+// }
+
+interface rectangle{
+    startX: number,
+    startY: number,
+    height: number,
+    width: number,
+}
+
+
 export function Canvas() {
 
-    const canvasRef = useRef(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
-    useEffect(() => {
-        
+    useEffect(() => {   
+        const canvas = canvasRef.current;
+        if(!canvas) {
+            return;
+        }
+        const ctx = canvas?.getContext("2d");
+        if(!ctx) return;
+
+        const shapesStore:rectangle[] = [];
+
+        canvas.width  = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        let isDrawing = false;
+        const startPoint = {x:0,  y:0};
+        const endPoint = {x:0, y: 0};
+
+        ctx.strokeStyle="red"
+
+        function getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent) {
+            // not needed as now canvas takes up entire window.innerWidth and height
+            const rect = canvas.getBoundingClientRect();
+            return {
+                x: evt.clientX - rect.left,
+                y: evt.clientY - rect.top
+            };
+        }
+
+        canvas.addEventListener("mousedown", (e) => {
+            const mousePos = getMousePos(canvas, e);
+            console.log('Mouse position: ' + mousePos.x + ',' + mousePos.y);
+            // now we draw a rectangle where mouse is clicked
+            // ctx.strokeRect(mousePos.x,mousePos.y,1,1);
+            startPoint.x = mousePos.x;
+            startPoint.y = mousePos.y;
+            endPoint.x = mousePos.x;
+            endPoint.y = mousePos.y;
+            isDrawing = true;
+        })
+        canvas.addEventListener("mousemove", (e) => {
+            // what do we do on mousemove:
+            // we clear any previous drawings:
+            // next we draw a new rectangle with new coordinates:
+            if(isDrawing) {
+                render();
+                const mousePos = getMousePos(canvas, e);
+                endPoint.x = mousePos.x;
+                endPoint.y = mousePos.y;
+                // here we need to do something to remove all previous drawings
+                ctx.strokeRect(startPoint.x,startPoint.y,endPoint.x-startPoint.x,endPoint.y-startPoint.y);
+            }
+        })
+
+        canvas.addEventListener("mouseup", (e) => {
+            //what to do on mousedown:
+            if(isDrawing) {
+                isDrawing = false;
+                // add the new shape to shape store:
+                const newShape:rectangle = {
+                    startX: startPoint.x,
+                    startY: startPoint.y,
+                    height: endPoint.x-startPoint.x,
+                    width: endPoint.y-startPoint.y, 
+                }
+                shapesStore.push(newShape)
+            }
+        })      
+        function render() {
+            if(!ctx || !canvas) return;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            shapesStore.map((shp) => {
+                ctx.strokeRect(shp.startX,shp.startY,shp.height, shp.width);
+            })
+        }
     }, [])
 
     return (
         <div>
-            This renders a canvas kinda object
-            <canvas ref={canvasRef} className="h-100 w-100 border-2 border-white">
+            <canvas ref={canvasRef} className="h-screen w-screen border-2 border-white">
             </canvas>
         </div>
     )
